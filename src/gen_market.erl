f95023d6e81e848660bec34e886d6ce42a8e5a2e
Will Vining <wfv@vining.dev>
2023-10-28 15:00:30 -0600

    First cut of generic market state machine

--- src/gen_market.erl
+++ src/gen_market.erl
@@ -8,7 +8,10 @@
 -record(data, {periodic = nil :: nil | #{},
                market_period :: undefined | timer:tref(),
                reservation_delay :: undefined | timer:tref(),
-               offer_delay :: undefined | timer:tref()}).
+               offer_delay :: undefined | timer:tref(),
+               buy_offers = [] :: [any()],
+               sell_offers = [] :: [any()],
+               mcp :: any()}).
 
 open_market(Market) ->
     gen_statem:cast(Market, open_market).
@@ -47,7 +50,80 @@ handle_event(cast, open_market, closed, #data{periodic = Periodic} = Data) ->
     {ReservationDelayTimer, OfferDelayTimer} = start_timers(Periodic),
     {next_state, open, Data#data{market_period = MarketPeriodTimer,
                                  reservation_delay = ReservationDelayTimer,
-                                 offer_delay = OfferDelayTimer}}.
+                                 offer_delay = OfferDelayTimer}};
+
+handle_event(cast, accept_reservations, open, Data) ->
+    {next_state, {accepting_reservations, {[], []}}, Data};
+
+handle_event(cast, accept_offers, {accepting_reservations, {BuyResv, SellResv}}, Data)
+  when length(BuyResv) > 0, length(SellResv) > 0 ->
+    {next_state, {accepting_offers, {BuyResv, SellResv}}, Data};
+handle_event(cast, accept_offers, {accepting_reservations, _}, Data) ->
+    {next_state, {market_done, {error, not_formed}}, Data};
+handle_event({call, From}, {make_reservation, buyer, Id}, {accepting_reservations, {BuyResv, SellResv}}, Data) ->
+    {next_state, {accepting_reservations, {[Id|BuyResv], SellResv}}, Data, [{reply, From, reservation_accepted}]};
+handle_event({call, From}, {make_reservation, seller, Id}, {accepting_reservations, {BuyResv, SellResv}}, Data) ->
+    {next_state, {accepting_reservations, {BuyResv, [Id|SellResv]}}, Data, [{reply, From, reservation_accepted}]};
+
+handle_event({call, From}, {make_offer, Id, Offer}, {accepting_offers, {Buyers, Sellers}}, Data) ->
+    case {lists:member(Id, Buyers), lists:member(Id, Sellers)} of
+        {true, _} ->
+            gen_statem:reply(From, offer_accepted),
+            NewData = add_buy_offer(Data, Offer),
+            NewBuyers = Buyers -- [Id],
+            NewSellers = Sellers;
+        {_, true} ->
+            gen_statem:reply(From, offer_accepted),
+            NewData = add_sell_offer(Data, Offer),
+            NewSellers = Sellers -- [Id],
+            NewBuyers = Buyers;
+        {false, false} ->
+            gen_statem:reply(From, offer_rejected),
+            NewData = Data,
+            NewSellers = Sellers,
+            NewBuyers = Buyers
+    end,
+    RemainingReservations = length(NewSellers) + length(NewBuyers),
+    if RemainingReservations =:= 0 ->
+            case clear_market(NewData#data.buy_offers, NewData#data.sell_offers) of
+                {ok, MCP} ->
+                    {next_state, {marked_done, cleared}, NewData#data{mcp = MCP}};
+                {error, Reason} ->
+                    {next_state, {market_done, {error, Reason}}, NewData}
+            end;
+       RemainingReservations > 0 ->
+            {next_state, {accepting_offers, {NewBuyers, NewSellers}}, NewData}
+    end;
+
+handle_event(cast, open_market, {market_done, _}, Data) ->
+    {ReservationDelayTimer, OfferDelayTimer} = start_timers(Data#data.periodic),
+    {next_state, open, Data#data{buy_offers = [],
+                                 sell_offers = [],
+                                 mcp = undefined,
+                                 offer_delay = OfferDelayTimer,
+                                 reservation_delay = ReservationDelayTimer}};
+handle_event(cast, open_market, State, Data) ->
+    logger:error("market failed to clear in alloted time~nState = ~p", [State]),
+    {ReservationDelayTimer, OfferDelayTimer} = start_timers(Data#data.periodic),
+    {next_state, open, Data#data{buy_offers = [],
+                                 sell_offers = [],
+                                 mcp = undefined,
+                                 offer_delay = OfferDelayTimer,
+                                 reservation_delay = ReservationDelayTimer}};
+
+handle_event({call, From}, {make_reservation, _, _}, _, _) ->
+    {keep_state_and_data, [{reply, From, reservation_rejected}]};
+handle_event({call, From}, {make_offer, _, _}, _, _) ->
+    {keep_state_and_data, [{reply, From, offer_rejected}]}.
+
+add_buy_offer(#data{buy_offers = BuyOffers} = Data, Offer) ->
+    Data#data{buy_offers = [Offer|BuyOffers]}.
+add_sell_offer(#data{sell_offers = SellOffers} = Data, Offer) ->
+    Data#data{sell_offers = [Offer|SellOffers]}.
+
+clear_market(BuyOffers, SellOffers) ->
+    %% TODO Implement a market clearing algorithm.
+    {ok, 0.0}.
 
 start_timers(#{reservation_delay := ReservationDelay,
                offer_delay := OfferDelay}) ->
