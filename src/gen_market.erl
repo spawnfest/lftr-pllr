49725c5cd99c50e86041d2b5ea2819e47eb48a2a
Will Vining <wfv@vining.dev>
2023-10-28 13:55:47 -0600

    Setup timers for market

--- src/gen_market.erl
+++ src/gen_market.erl
@@ -2,13 +2,22 @@
 
 -behaviour(gen_statem).
 
--export([start_link/2, open_market/1]).
+-export([start_link/2, open_market/1, accept_reservations/1, accept_offers/1]).
 -export([callback_mode/0, init/1, handle_event/4]).
 
--record(data, {periodic = nil :: nil | #{}}).
+-record(data, {periodic = nil :: nil | #{},
+               market_period :: undefined | timer:tref(),
+               reservation_delay :: undefined | timer:tref(),
+               offer_delay :: undefined | timer:tref()}).
 
 open_market(Market) ->
     gen_statem:cast(Market, open_market).
+
+accept_reservations(Market) ->
+    gen_statem:cast(Market, accept_reservations).
+
+accept_offers(Market) ->
+    gen_statem:cast(Market, accept_offers).
 
 start_link(Id, Options) ->
     gen_statem:start_link({local, Id}, ?MODULE, Options, []).
@@ -29,5 +38,21 @@ periodic_options(Options) ->
 
 callback_mode() -> handle_event_function.
 
-handle_event(cast, open_market, closed, Data) ->
-    {next_state, accept_reservations, Data}.
+handle_event(cast, open_market, closed, #data{periodic = nil} = Data) ->
+    {next_state, open, Data};
+handle_event(cast, open_market, closed, #data{periodic = Periodic} = Data) ->
+    {ok, MarketPeriodTimer} =
+        timer:apply_interval(
+          maps:get(market_period, Periodic), gen_market, open_market, [self()]),
+    {ReservationDelayTimer, OfferDelayTimer} = start_timers(Periodic),
+    {next_state, open, Data#data{market_period = MarketPeriodTimer,
+                                 reservation_delay = ReservationDelayTimer,
+                                 offer_delay = OfferDelayTimer}}.
+
+start_timers(#{reservation_delay := ReservationDelay,
+               offer_delay := OfferDelay}) ->
+    {ok, ReservationDelayTimer} =
+        timer:apply_after(ReservationDelay, gen_market, accept_reservations, [self()]),
+    {ok, OfferDelayTimer} =
+        timer:apply_after(OfferDelay, gen_market, accept_offers, [self()]),
+    {ReservationDelayTimer, OfferDelayTimer}.
