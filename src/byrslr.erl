1cd642c1381d848220394e1c87baf7b193f417cc
Will Vining <wfv@vining.dev>
2023-10-29 16:49:50 -0600

    Add API functions for event-driven markets to `byrslr` module

--- src/byrslr.erl
+++ src/byrslr.erl
@@ -1,6 +1,7 @@
 -module(byrslr).
 
--export([new_market/2, join_market/1, make_buy_reservation/1, make_sell_reservation/1, make_offer/3]).
+-export([new_market/2, join_market/1, make_buy_reservation/1, make_sell_reservation/1, make_offer/3,
+         start_new_cycle/1, start_accepting_offers/1, start_accepting_reservations/1]).
 
 %% @doc Start a new market.
 -spec new_market(Id :: atom(), Options :: proplists:proplist()) -> ok | {error, market_exists}.
@@ -28,3 +29,15 @@ make_sell_reservation(MarketId) ->
 %% @doc Make an offer corresponding to the reservation `ReservationId'.
 make_offer(MarketId, ReservationId, Offer) ->
     gen_market:make_offer(MarketId, ReservationId, Offer).
+
+%% @doc Signal that `Market' should start a new cycle.
+start_new_cycle(Market) ->
+    gen_market:open_market(Market).
+
+%% @doc Signal that `Market' should start accepting reservations.
+start_accepting_reservations(Market) ->
+    gen_market:accept_reservations(Market).
+
+%% @doc Signal that `Market' should start accepting offers
+start_accepting_offers(Market) ->
+    gen_market:accept_offers(Market).
